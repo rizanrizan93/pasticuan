54fcc2b70a8ceb5404beed247244879ae87ee260
rizanrizan93 <rizanrizan93@gmail.com>
2026-08-15 19:20:59 +0700

    Use unambiguous company-only entity fixture

--- tests/test_live_forward_company_identity.py
+++ tests/test_live_forward_company_identity.py
@@ -12,13 +12,13 @@ class _Response:
 
 def test_company_name_can_resolve_event_without_ticker_in_title(monkeypatch):
     xml = """<rss><channel><item>
-    <title>Mark Dynamics catat pesanan hingga kuartal III dan kapasitas penuh</title>
-    <link>https://example.com/mark</link>
+    <title>Jayamas Medica ekspansi kapasitas dan luncurkan produk baru</title>
+    <link>https://example.com/onemed</link>
     <pubDate>Fri, 14 Aug 2026 08:00:00 GMT</pubDate><source>Example</source>
     </item></channel></rss>"""
     monkeypatch.setattr(lfe.requests, 'get', lambda *a, **k: _Response(xml))
     frame = lfe.collect_live_forward_evidence(
-        ['MARK.JK'], company_names={'MARK.JK': 'MARK DYNAMICS INDONESIA Tbk, PT'}, max_workers=1,
+        ['OMED.JK'], company_names={'OMED.JK': 'JAYAMAS MEDICA INDUSTRI Tbk, PT'}, max_workers=1,
     )
     assert frame.iloc[0]['forward_collection_state'] == 'MATERIAL_FORWARD_RESEARCH_EVIDENCE_FOUND'
     assert frame.iloc[0]['entity_match_method'].startswith('COMPANY_TOKEN_MATCH_')
